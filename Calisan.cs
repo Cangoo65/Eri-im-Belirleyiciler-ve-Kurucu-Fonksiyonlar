3e1baf3df05f031e46059dc54d278d416c53532d
Cangoo65 <caneraytekin06@hotmail.com>
2024-02-18 21:24:37 +0300

    Proje dosyası ekle.

---
+++ Calisan.cs
@@ -0,0 +1,25 @@
+﻿namespace Erişim_Belirleyiciler_ve_Kurucu_Fonksiyonlar
+{
+    internal class Calisan
+    {
+        public string Ad;
+        public string Soyad;
+        public int No;
+        public string Departman;
+
+        public Calisan(string ad, string soyad, int no, string departman)
+        {
+            this.Ad = ad;
+            this.Soyad = soyad;
+            this.No = no;
+            this.Departman = departman;
+        }
+        public void CalisanBilgileriGoster()
+        {
+            Console.WriteLine("Çalışanın Adı :" + Ad);
+            Console.WriteLine("Çalışanın Soyadı :" + Soyad);
+            Console.WriteLine("Çalışanın Numarası :" + No);
+            Console.WriteLine("çalışanın Departmanı :" + Departman);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using Erişim_Belirleyiciler_ve_Kurucu_Fonksiyonlar;
+
+Calisan calisan = new("Ali", "Konak", 11223344, "İnsan Kaynakları");
+calisan.CalisanBilgileriGoster();
+Calisan calisan1 = new("Dilek", "Kaynak", 99887777, "Satış");
+calisan1.CalisanBilgileriGoster();
+Calisan calisan2 = new("Ahmet", "Yamak",33557711,"Yeme içme");
+calisan2.CalisanBilgileriGoster();
+
+Console.Read();
